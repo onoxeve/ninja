07cf811adbcb8ece6ccc68548355b4c3e1a8459c
onoxeve <onoxeve@gmail.com>
2018-12-22 08:49:06 +0900

    modify: adsense認証用のtag

--- src/html.js
+++ src/html.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// TODO: 認証完了後に消す
+import AdSense from 'react-adsense';
+
 export default class HTML extends React.Component {
   render() {
     return (
@@ -26,6 +29,10 @@ export default class HTML extends React.Component {
           />
           {this.props.headComponents}
           <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <AdSense.Google
+            client='ca-pub-4357144858136704'
+            slot=''
+          />
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
